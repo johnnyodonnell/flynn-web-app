cff7efeafc05f693f7c2b41d24a8ebcb5287ef98
Johnny O'Donnell <odjohnny@amazon.com>
2021-03-07 20:26:17 -0800

    Simple server that redirects to HTTPS

---
+++ index.rkt
@@ -0,0 +1,39 @@
+#lang racket
+
+(require web-server/servlet
+         web-server/servlet-env)
+
+
+(define get-host
+  (lambda (headers)
+    (cond
+      [(empty? headers) #f]
+      [(equal? (caar headers) 'host) (cdar headers)]
+      [else (get-host (cdr headers))])))
+
+(define redirect
+  (lambda (req)
+    (redirect-to
+      (string-append
+        "https://"
+        (get-host (request-headers req))
+        (url->string (request-uri req))))))
+
+(thread
+  (lambda ()
+    (serve/servlet redirect
+                   #:port 80
+                   #:launch-browser? #f
+                   #:servlet-regexp #rx""
+                   #:quit? #f)))
+
+(define app
+  (lambda (req)
+    (response/xexpr "Hello World")))
+
+(serve/servlet app
+               #:port 443
+               #:launch-browser? #f
+               #:servlet-regexp #rx""
+               #:quit? #f)
+
