7f9aa63e312b8c2e52098ff9a0305682da2792fb
Johnny O'Donnell <odjohnny@amazon.com>
2021-03-08 20:02:03 -0800

    Only include 1 server and plan to use Nginx

--- index.rkt
+++ index.rkt
@@ -4,38 +4,12 @@
          web-server/servlet-env)
 
 
-(define get-host
-  (lambda (headers)
-    (cond
-      [(empty? headers) #f]
-      [(equal? (caar headers) 'host) (cdar headers)]
-      [else (get-host (cdr headers))])))
-
-(define redirect
-  (lambda (req)
-    (redirect-to
-      (string-append
-        "https://"
-        (get-host (request-headers req))
-        (url->string (request-uri req)))
-      301)))
-
-(thread
-  (lambda ()
-    (serve/servlet redirect
-                   #:port 80
-                   #:launch-browser? #f
-                   #:servlet-regexp #rx""
-                   #:quit? #f)))
-
 (define app
   (lambda (req)
     (response/xexpr "Hello World")))
 
 (serve/servlet app
-               #:port 443
                #:launch-browser? #f
                #:servlet-regexp #rx""
-               #:quit? #f
-               #:ssl? #t)
+               #:quit? #f)
 
